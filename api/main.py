2b90b56a92357fb7563e522946272a30fdf7cdb2
theplaceincan <abrahatatsbeha@gmail.com>
2026-04-04 10:38:36 -0700

    connected fastapi to react frontend

--- api/main.py
+++ api/main.py
@@ -1,7 +1,43 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["http://localhost:5173"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"]
+                   )
+
 @app.get("/")
 def root():
-  return {"message": "Hello world"}
+  return {"message": "Home route, hello!"}
+
+@app.get("/telemetry")
+def root():
+  return {
+    # // test data 
+    "drones" : [
+      { 
+       "id": 1, 
+       "position": {
+         "lng": 10.0, 
+         "lat": 20.0, 
+         "alt": 0.0
+        },
+       "battery" : 92,
+       "state": "IDLE"
+       },
+      { 
+       "id": 2, 
+       "position": {
+         "lng": 20.0, 
+         "lat": 30.0, 
+         "alt": 70.0
+        },
+       "battery" : 67,
+       "state": "DELIVERING"
+      }
+    ]
+  }
--- dashboard/display/src/App.tsx
+++ dashboard/display/src/App.tsx
@@ -1,12 +1,58 @@
 import './App.css'
 
-function App() {
+import { useEffect, useState } from "react";
+
+type Drone = {
+  id: number;
+  position: {
+    lng: number;
+    lat: number;
+    alt: number;
+  };
+  battery: number;
+  state: string;
+};
+
+export default function App() {
+  const [drones, setDrones] = useState<Drone[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("http://127.0.0.1:8000/telemetry")
+      .then((response) => response.json())
+      .then((data) => {
+        setDrones(data.drones);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Failed to fetch telemetry:", err);
+        setError("Failed to fetch telemetry");
+        setLoading(false);
+      });
+    }, 1000)
+    
+    return () => clearInterval(interval);
+  }, []);
+
+  if (loading) { return <p>Loading telemetry...</p>; }
+  if (error) { return <p>{error}</p>; }
 
   return (
-    <>
-      <p>Fleet Telemetry Display</p>
-    </>
-  )
-}
+    <div>
+      <h1>Fleet Telemetry Dashboard</h1>
 
-export default App
+      {drones.map((drone) => (
+        <div key={drone.id}>
+          <h2>Drone #{drone.id}</h2>
+          <p>Longitude: {drone.position.lng}</p>
+          <p>Latitude: {drone.position.lat}</p>
+          <p>Altitude: {drone.position.alt}</p>
+          <p>Battery: {drone.battery}%</p>
+          <p>State: {drone.state}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
